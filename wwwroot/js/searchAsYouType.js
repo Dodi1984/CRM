b15cf9f54734303795edca5e6aa99c06f21706b7
dodi1984 <dan.alexandru.niculescu@gmail.com>
2016-09-25 12:37:34 +0300

    Ready for javascript

--- wwwroot/js/searchAsYouType.js
+++ wwwroot/js/searchAsYouType.js
@@ -1,17 +1,3 @@
-$(document).ready(function () {
-/* initially hide list items */
-$("#companies td").hide();
-console.log("PLM")
-/* filter dinosaurs as you type */
-$("#name").on("keyup click input", function () {
-if (this.value.length > 0) {
-  $("#companies td").hide().filter(function () {
-    return $(this).text().toLowerCase().indexOf($("#name").val().toLowerCase()) != -1;
-  }).show();
-}
-else {
-  $("#companies td").hide();
-}
-});
-
-});
+var onFilterChange = function() {
+    console.log("Test");
+};
