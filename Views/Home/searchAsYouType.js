b9dfb46971b61f2fd8d3855232e82ba66c0e4da4
dodi1984 <dan.alexandru.niculescu@gmail.com>
2016-09-25 19:52:28 +0300

    SearchAsYouType DONE

--- Views/Home/searchAsYouType.js
+++
@@ -1,3 +0,0 @@
-var onFilterChange = function() {
-    console.log("Test");
-};
--- wwwroot/js/searchAsYouType.js
+++ wwwroot/js/searchAsYouType.js
@@ -1,3 +1,24 @@
-var onFilterChange = function() {
-    console.log("Test");
-};
+$(document).ready(function () {
+  App.init()
+})
+
+var App = {
+  tableRows: null,
+  init: function () {
+    this.tableRows = document.getElementsByClassName('info')
+  },
+  onFilterChange: function () {
+    var filterText = document.getElementById('filter').value    
+    // console.log(filterText)
+    // this.tableRows[0].style.display = 'none' // for hiding elements
+    for (i = 0; i < this.tableRows.length;i++) {
+      if (this.tableRows[i].innerText.toUpperCase().indexOf(filterText.toUpperCase()) === -1) {
+        this.tableRows[i].style.display = 'none'
+      }
+      else{
+          console.log("a intart in else")
+        this.tableRows[i].style.display = ""      
+      }
+    }
+  }
+}
